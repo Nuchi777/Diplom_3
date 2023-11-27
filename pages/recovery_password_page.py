60a2adf9d68822d7b091c639aa6bf1a5cc2053dd
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-11-27 19:24:06 +0300

    rename test check_show_or_hide_password_button_is_active

--- pages/recovery_password_page.py
+++ pages/recovery_password_page.py
@@ -24,5 +24,5 @@ class RecoveryPasswordPage(BasePage):
     def click_on_show_or_hide_password_button(self):
         self.element_is_visible(self.locators.BUTTON_SHOW_OR_HIDE_PASSWORD).click()
 
-    def check_show_or_hide_password_button(self):
+    def check_show_or_hide_password_button_is_active(self):
         assert self.element_is_presence(self.locators.PASSWORD_FIELD_ACTIVE)
